dd2d0e64fadf0322b5d9cfca94d5babbd1762a9b
Brux <okladno94@gmail.com>
2024-05-19 00:28:38 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active " + active);
+        System.out.println("Status " + status);
+        System.out.println("Message " + message);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error1 = new Error(true, 2, "error, try again");
+        Error error2 = new Error(false, 1, "error, check connection");
+        error.printInfo();
+        error1.printInfo();
+        error2.printInfo();
+    }
+}
