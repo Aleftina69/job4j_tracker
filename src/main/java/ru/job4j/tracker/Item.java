e8cf573cb574096f95ccb3b0017d7921c70e1ee0
Brux <okladno94@gmail.com>
2024-05-22 15:52:06 +0500

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
@@ -32,4 +35,10 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+        LocalDateTime created = LocalDateTime.now();
+
+        public LocalDateTime getCreated() {
+            return created;
+        }
 }
