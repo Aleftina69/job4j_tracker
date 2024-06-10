c0219cb552e389fdbd4f6d9114aec5c4b5075c60
Brux <okladno94@gmail.com>
2024-06-10 15:57:17 +0500

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,22 @@
+package ru.job4j.ex;
+
+public class FindEl extends Exception {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                return i;
+            }
+        }
+        throw new ElementNotFoundException("Element not found");
+    }
+
+    @SuppressWarnings("checkstyle:NoWhitespaceAfter")
+    public static void main(String[] args) {
+        String[] value = {"Привет, ", "как ", "дела?"};
+        try {
+            System.out.println(indexOf(value, "как "));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
