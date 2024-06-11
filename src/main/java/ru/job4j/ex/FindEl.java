cd9767bef523d860aaaa47ef0232e005b211d76b
Brux <okladno94@gmail.com>
2024-06-11 10:51:39 +0500

    0.4. Пользовательские исключения. [#219367 #500115]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,6 +1,6 @@
 package ru.job4j.ex;
 
-public class FindEl extends Exception {
+public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
