44ea0cab79b1c210839ad85bed57f5c4f359569b
Brux <okladno94@gmail.com>
2024-07-15 16:23:28 +0500

    7. Конвертация двумерного массива в ArrayList [#48580 #505156]

--- src/main/java/ru/job4j/collection/ConvertList2Array.java
+++ src/main/java/ru/job4j/collection/ConvertList2Array.java
@@ -5,18 +5,18 @@ import java.util.List;
 public class ConvertList2Array {
     public static int[][] toArray(List<Integer> list, int cells) {
         int groups = (int) Math.ceil((double) list.size() / cells);
-        int[][] array = new int[groups][cells];
+        int[][] arrays = new int[groups][cells];
         int row = 0;
         int cell = 0;
         for (Integer number : list) {
-            array[row][cell] = number;
+            arrays[row][cell] = number;
             cell++;
             if (cell == cells) {
                 cell = 0;
                 row++;
             }
         }
-        return array;
+        return arrays;
     }
 
     public static void main(String[] args) {
