434957b0ee08a5559e76542e30644abb92fb0ea6
Brux <okladno94@gmail.com>
2024-07-21 23:58:22 +0500

    0. Map, HashMap [#212652 #505920]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("okladno94@gmail.com", "Тишакова Алефтина");
         map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("qwerty100@mail.ru", "Ivan Ivanov");
+        map.put("asddfghj67@mail.ru", "Сергей Петров");
+        map.put("okladno94@gmail.com", "Aleftina Tishakova");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
