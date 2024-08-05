f980d55bd59cad196628fffd66fdcb75ceaa8b8b
Brux <okladno94@gmail.com>
2024-08-05 23:29:46 +0500

    7. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -25,14 +25,3 @@ public class PhoneDictionary {
         return result;
     }
 }
-
-       /* ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
-            if (person.getName().contains(key) || person.getSurname().contains(key)
-                    || person.getPhone().contains(key) || person.getAddress().contains(key)) {
-                result.add(person);
-            }
-        }
-        return result;
-    }
-}*/
