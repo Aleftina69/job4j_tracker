a3d28b3ab1569e60c278b19f1dafbaae81222022
Brux <okladno94@gmail.com>
2024-08-07 22:29:45 +0500

    5. Фильтрация учеников [#110058 #507948]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -11,19 +11,4 @@ public class School {
                 .filter(predicate)
                 .collect(Collectors.toList());
     }
-
-    public List<Student> getGroup10A(List<Student> students) {
-        Predicate<Student> group10A = student -> student.getScore() >= 70 && student.getScore() <= 100;
-        return collect(students, group10A);
-    }
-
-    public List<Student> getGroup10B(List<Student> students) {
-        Predicate<Student> group10B = student -> student.getScore() >= 50 && student.getScore() < 70;
-        return collect(students, group10B);
-    }
-
-    public List<Student> getGroup10V(List<Student> students) {
-        Predicate<Student> group10V = student -> student.getScore() > 0 && student.getScore() < 50;
-        return collect(students, group10V);
-    }
 }
