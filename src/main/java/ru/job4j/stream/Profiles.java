701917b4756eda884165b9e94f2bc7e0c454facd
Brux <okladno94@gmail.com>
2024-08-07 22:33:34 +0500

    6. Список адресов [#110062 #507963]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,6 +1,5 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,7 +7,6 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        List<Address> rslt = new ArrayList<>();
         return profiles.stream()
                 .map(Profile::getAddress)
                 .collect(Collectors.toList());
